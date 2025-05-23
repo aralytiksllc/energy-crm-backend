0b532f9f1a95f595104913aa2f68e8b02a9264fa
kreshnik.alidema <kreshnikalidema@gmail.com>
2025-05-23 23:46:55 +0200

    refactor: simplify database configuration and remove redundant files

--- sequelize.config.js
+++ sequelize.config.js
@@ -1,5 +1,20 @@
 require('ts-node/register');
+require('dotenv').config();
 
-const { databaseConfig } = require('./src/configs/database.config');
+const config = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  dialect: 'postgres',
+  schema: process.env.DB_SCHEMA,
+  migrationStorage: 'sequelize',
+  seederStorage: 'sequelize'
+};
 
-module.exports = databaseConfig;
+module.exports = {
+  development: config,
+  test: config,
+  production: config
+};

326e71edd0f991626d701b35e48274d9f5e48134
Kreshnik Alidema <kreshnik.alidema@gmail.com>
2025-08-29 10:39:11 +0200

    Puppeteer Heroku: chrome buildpack + executablePath + no-sandbox

--- src/modules/contracts/commands/puppeteer.service.ts
+++ src/modules/contracts/commands/puppeteer.service.ts
@@ -1,28 +1,40 @@
-import { Injectable, OnModuleDestroy } from '@nestjs/common';
+// puppeteer.service.ts
+import { Injectable, OnModuleDestroy, Logger } from '@nestjs/common';
 import puppeteer, { Browser } from 'puppeteer';
 
 @Injectable()
 export class PuppeteerService implements OnModuleDestroy {
   private browser?: Browser;
   private launching?: Promise<Browser>;
+  private readonly logger = new Logger(PuppeteerService.name);
 
   async getBrowser(): Promise<Browser> {
     if (this.browser) return this.browser;
-    if (!this.launching) {
-      this.launching = puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      });
-    }
+
+    const executablePath =
+      process.env.PUPPETEER_EXECUTABLE_PATH ||
+      process.env.CHROME_BIN ||
+      undefined;
+
+    this.logger.log(`Launching Chrome at: ${executablePath ?? '(bundled)'}`);
+
+    this.launching = puppeteer.launch({
+      headless: true,
+      executablePath,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--font-render-hinting=none',
+      ],
+    });
+
     this.browser = await this.launching;
     return this.browser;
   }
 
   async onModuleDestroy() {
-    if (this.browser) {
-      await this.browser.close();
-      this.browser = undefined;
-      this.launching = undefined;
-    }
+    await this.browser?.close();
+    this.browser = undefined;
+    this.launching = undefined;
   }
 }
